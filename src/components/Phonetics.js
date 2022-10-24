55f5f4e67937d516a46b0a449a1e2ac0658c8c3b
Anna <anna.svrva@gmail.com>
2022-10-24 19:55:36 +0300

    built phonetic button

--- src/components/Phonetics.js
+++ src/components/Phonetics.js
@@ -3,16 +3,26 @@ import React from "react";
 import "../styles/Phonetics.css";
 
 export default function Phonetics(props) {
-  return (
-    <div className="Phonetics">
-      <div className="row mb-3">
-        <div className="sound col-2 ">
-          <a href={props.phone.audio} target="_blank" rel="noreferrer">
-            <i className="fa-solid fa-volume-low"></i>
-          </a>
+  let audio = new Audio(props.phone.audio);
+
+  const start = () => {
+    audio.play();
+  };
+
+  if (props.phone.audio === "" || props.phone.text === "") {
+    return null;
+  } else {
+    return (
+      <div className="Phonetics">
+        <div className="row mb-3">
+          <div className="sound col-2">
+            <button className="audioButton" onClick={start}>
+              <i className="fa-solid fa-volume-low"></i>
+            </button>
+          </div>
+          <div className="sound-text col-10 ps-3">{props.phone.text}</div>
         </div>
-        <div className="sound-text col-10 ps-3">{props.phone.text}</div>
       </div>
-    </div>
-  );
+    );
+  }
 }
